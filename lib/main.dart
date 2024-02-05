fb9179682e4e326ba658f6641f3802650fd2609a
Ken Lee <ahkenlee@yahoo.com.hk>
2024-02-05 12:54:25 +0800

    Animation removes after timer expires

--- lib/main.dart
+++ lib/main.dart
@@ -20,20 +20,55 @@ class SpriteBatchLoadExample extends FlameGame {
   @override
   Future<void> onLoad() async {
     final spriteSize = Vector2(128.0, 128.0);
+    const spriteNum = 8;
     boomSprite = SpriteSheet(
       image: await images.load('boom.png'),
       srcSize: spriteSize,
     );
 
-    for (double i = 0 ; i <=7 ; i++) {
-    final animation = boomSprite.createAnimation(row: i.round(), stepTime: 0.2);
-    final boomComponent = SpriteAnimationComponent(
-      animation: animation,
-      scale: Vector2(2, 2),
-      size: spriteSize,
-      position: spriteSize * i,
-    );
-    add(boomComponent);
+    for (double i = 0; i < spriteNum; i++) {
+      add(AnimatedBoom(
+        boomSprite: boomSprite,
+        atPosition: spriteSize * i,
+        spriteSize: spriteSize,
+        row: i.round(),
+        num: spriteNum,
+      ));
+    }
+  }
+}
+
+class AnimatedBoom extends SpriteAnimationComponent {
+  final SpriteSheet boomSprite;
+  final Vector2 atPosition;
+  final Vector2 spriteSize;
+  final int row;
+  final int num;
+  late Timer lifeTime;
+
+  AnimatedBoom({
+    required this.boomSprite,
+    required this.atPosition,
+    required this.spriteSize,
+    required this.row,
+    required this.num,
+  });
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    scale = Vector2(2, 2);
+    position = atPosition;
+    animation = boomSprite.createAnimation(row: row, stepTime: 0.2);
+    lifeTime = Timer(num * 0.2);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    lifeTime.update(dt);
+    if (lifeTime.finished) {
+      removeFromParent();
     }
   }
 }
